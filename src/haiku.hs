461f53892f5630b86e91ae3aebe0f9c79a64dc33
watarui <wataruby@gmail.com>
2019-09-24 23:24:21 +0900

    bracket

--- src/haiku.hs
+++ src/haiku.hs
@@ -1,5 +1,14 @@
 import           System.IO
+import           Control.Exception
 
-main = withFile "haiku.txt" ReadMode $ \handle -> do
+main = withFile' "haiku.txt" ReadMode $ \handle -> do
     contents <- hGetContents handle
     putStr contents
+
+
+-- bracket :: IO a -> (a -> IO b) -> (a -> IO c) -> IO c
+-- bracket の第二引数は例外が投げられても呼ばれる
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' name mode f = bracket (openFile name mode)
+                                (\handle -> hClose handle)
+                                (\handle -> f handle)
